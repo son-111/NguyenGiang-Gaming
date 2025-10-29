b11de4864c4856a309f89b40261cc4286ceff5c0
Giang11 <minhsonn033@gmail.com>
2025-10-30 02:00:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ function handleLogin() {
   const user = document.getElementById("username").value.trim();
   const pass = passwordInput.value.trim();
 
-  if (user === "a" && pass === "a") {
+  if (user === "abcdef80" && pass === "Zxc1230@@") {
     adminLevel = 1;
     showCustomAlert("✅ Đăng nhập thành công!");
     afterLogin();
@@ -328,3 +328,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
+
